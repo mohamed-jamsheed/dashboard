d33247c4a58eff9524d14735d3b809bf28b63811
mohamed-jamsheed <mohamedjamsheed50@gmail.com>
2023-01-14 13:07:39 +0530

    dashboard creation

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,20 @@
+import { EventEmitter } from '@angular/core';
+import { Output } from '@angular/core';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+  @Output() isSideBarToggle=new EventEmitter()
+  
+  
+  sideBarToggle(){
+    this.isSideBarToggle.emit()
+    setTimeout(()=>{
+      window.dispatchEvent(new Event('resize'))
+    },300);
+  }
+}
